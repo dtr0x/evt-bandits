03b555c429211814a9805b4452be24d6c0eedfcc
Dylan Troop <dylantroop@gmail.com>
2020-12-10 01:36:00 +0000

    Remove shell scripts.

--- fraction_closer.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-dirname=$1
-
-mkdir -p fraction_closer/$dirname/sa fraction_closer/$dirname/ev fraction_closer/plots
-
-job_ID=$(sbatch --output=/dev/null --parsable fraction_closer_search.sh $dirname)
-sbatch --output=/dev/null --depend=afterany:${job_ID} fraction_closer_plots.sh $dirname
